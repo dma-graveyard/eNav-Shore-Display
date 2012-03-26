30db20858b41336ee65feac0500f31c5d227ee07
DaC <dac@dacUbuntu.(none)>
2012-03-26 16:58:18 +0200

    Revert "Revert "Added multiple map frames instead of one""
    This reverts commit 639d43965eabd4649fee085c211423dc7fa35814.

--- src/main/java/dk/frv/enav/esd/gui/ChartPanel.java
+++ src/main/java/dk/frv/enav/esd/gui/ChartPanel.java
@@ -80,7 +80,7 @@ public class ChartPanel extends OMComponentPanel implements MouseWheelListener {
 	public ChartPanel() {
 		super();
 		// Set map handler
-		mapHandler = ESD.getMapHandler();
+		mapHandler = new MapHandler();
 		// Set layout
 		setLayout(new BorderLayout());
 		// Set border
