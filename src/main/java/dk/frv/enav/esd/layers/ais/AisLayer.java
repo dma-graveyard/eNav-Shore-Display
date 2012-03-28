6dfc7518bd2bedacab8644f6c8a0898913156d89
Claes Ladefoged <claesnl@gmail.com>
2012-03-28 16:58:52 +0200

    Completed display of MMSI, Name and Call Id around vessels.

--- src/main/java/dk/frv/enav/esd/layers/ais/AisLayer.java
+++ src/main/java/dk/frv/enav/esd/layers/ais/AisLayer.java
@@ -1,6 +1,7 @@
 package dk.frv.enav.esd.layers.ais;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,8 @@ import com.bbn.openmap.layer.OMGraphicHandlerLayer;
 import com.bbn.openmap.omGraphics.OMGraphic;
 import com.bbn.openmap.omGraphics.OMGraphicList;
 import com.bbn.openmap.omGraphics.OMPoly;
+import com.bbn.openmap.omGraphics.OMText;
+import com.bbn.openmap.omGraphics.labeled.LabeledOMGraphic;
 
 import dk.frv.ais.geo.GeoLocation;
 import dk.frv.ais.message.AisMessage;
@@ -17,6 +20,9 @@ import dk.frv.enav.esd.ais.AisHandler.AisMessageExtended;
 import dk.frv.enav.esd.ais.VesselAisHandler;
 import dk.frv.enav.esd.gui.ChartPanel;
 import dk.frv.enav.esd.nmea.IVesselAisListener;
+import dk.frv.enav.ins.ais.VesselPositionData;
+import dk.frv.enav.ins.ais.VesselStaticData;
+import dk.frv.enav.ins.ais.VesselTarget;
 
 public class AisLayer extends OMGraphicHandlerLayer implements Runnable, IVesselAisListener {
 	private OMGraphic graphic = new OMGraphicList();
@@ -24,6 +30,14 @@ public class AisLayer extends OMGraphicHandlerLayer implements Runnable, IVessel
 	private OMGraphicList list = new OMGraphicList();
 	private Map<Long, OMPoly> targets = new HashMap<Long, OMPoly>();
 	private static VesselAisHandler aisHandler;
+	
+	private VesselPositionData location;
+	private OMPoly poly;
+	private Font font = null;
+	private OMText label = null;
+	private int sizeOffset = 5;
+	private String callSign;
+	private String name;
 
 	@Override
 	public void run() {
@@ -40,22 +54,77 @@ public class AisLayer extends OMGraphicHandlerLayer implements Runnable, IVessel
 
 	private void drawVessels() {
 		if (aisHandler != null) {
-			int[] xPos = { -5, 0, 5, -5 };
-			int[] yPos = { -5, 5, -5, -5 };
 			list.clear();
+			
+			// Size of triangle
+			int[] xPos = { -sizeOffset, 0, sizeOffset, -sizeOffset };
+			int[] yPos = { -sizeOffset, sizeOffset, -sizeOffset, -sizeOffset };
 			
 			List<AisMessageExtended> shipList = aisHandler.getShipList();
 			for (int i = 0; i < shipList.size(); i++) {
 				if (aisHandler.getVesselTargets().containsKey(shipList.get(i).MMSI)) {
-					GeoLocation theShip = aisHandler.getVesselTargets().get(shipList.get(i).MMSI).getPositionData().getPos();
-					OMPoly poly = new OMPoly(theShip.getLatitude(), theShip.getLongitude(), xPos, yPos, 0);
+					AisMessageExtended vessel = shipList.get(i);
+					VesselTarget vesselTarget = aisHandler.getVesselTargets().get(vessel.MMSI);
+					VesselStaticData staticData = vesselTarget.getStaticData();
+					font = new Font(Font.SANS_SERIF, Font.PLAIN, 11);
+					location = vesselTarget.getPositionData();
+					double lat = location.getPos().getLatitude();
+					double lon = location.getPos().getLongitude();
+					
+					// Draw vessel
+					poly = new OMPoly(location.getPos().getLatitude(), location.getPos().getLongitude(), xPos, yPos, 0);
 					poly.setFillPaint(new Color(0));
 					list.add(poly);
+					
+					// Add MMSI sign
+				    label = new OMText(0, 0, 0, 0, Long.toString(shipList.get(i).MMSI), font, OMText.JUSTIFY_CENTER);
+				    label.setLat(lat);
+				    label.setLon(lon);
+				    label.setY(4*sizeOffset);
+				    label.setData("ID: " + Long.toString(vesselTarget.getMmsi()));
+				    list.add(label);
+				    
+				    if(staticData != null){
+				    	callSign = staticData.getCallsign();
+				    	name = staticData.getName();
+				    } else {
+				    	callSign = "N/A";
+				    	name = "N/A";
+				    }
+				    
+				    // Add call sign
+				    label = new OMText(0, 0, 0, 0, Long.toString(shipList.get(i).MMSI), font, OMText.JUSTIFY_RIGHT);
+				    label.setLat(lat);
+				    label.setLon(lon);
+				    label.setX(-2*sizeOffset);
+				    label.setY(sizeOffset);
+				    label.setData("Call Sign: " + callSign);
+				    list.add(label);
+				    
+				    // Add call sign
+				    label = new OMText(0, 0, 0, 0, Long.toString(shipList.get(i).MMSI), font, OMText.JUSTIFY_LEFT);
+				    label.setLat(lat);
+				    label.setLon(lon);
+				    label.setX(2*sizeOffset);
+				    label.setY(sizeOffset);
+				    label.setData("Name: " + name);
+				    list.add(label);
+				    
+				    // Add heading
+				    float trueHeading = location.getTrueHeading();
+				    System.out.println(trueHeading);
+				    boolean noHeading = false;
+			        if (trueHeading == 511) {
+			            trueHeading = location.getCog();
+			            noHeading = true;
+			        }
+			        
+				    
+				    
 				}
 			}
 			doPrepare();
 		}
-		
 	}
 
 	@Override
