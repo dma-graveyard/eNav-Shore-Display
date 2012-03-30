50587e84c3aee35837ffcda3c0ba59679d991589
Claes Ladefoged <claesnl@gmail.com>
2012-03-31 00:39:36 +0200

    Added (static) info menu for vessel portrayal

--- src/main/java/dk/frv/enav/esd/layers/ais/AisLayer.java
+++ src/main/java/dk/frv/enav/esd/layers/ais/AisLayer.java
@@ -1,10 +1,26 @@
 package dk.frv.enav.esd.layers.ais;
 
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingUtilities;
+import javax.swing.table.TableColumn;
 
 import com.bbn.openmap.layer.OMGraphicHandlerLayer;
 import com.bbn.openmap.omGraphics.OMGraphic;
@@ -30,7 +46,8 @@ public class AisLayer extends OMGraphicHandlerLayer implements Runnable, IVessel
 	private OMGraphicList list = new OMGraphicList();
 	private Map<Long, OMPoly> targets = new HashMap<Long, OMPoly>();
 	private static VesselAisHandler aisHandler;
-	
+	private List<AisMessageExtended> shipList;
+
 	private VesselPositionData location;
 	private OMPoly poly;
 	private Font font = null;
@@ -38,9 +55,17 @@ public class AisLayer extends OMGraphicHandlerLayer implements Runnable, IVessel
 	private int sizeOffset = 5;
 	private String callSign;
 	private String name;
+	private String data[][];
+	private Container pane;
+	private JTable table;
+	private JTextArea tf2 = new JTextArea();
+	private JFrame jf;
 
 	@Override
 	public void run() {
+
+		ESD.sleep(1000);
+		shipInfoWindow();
 
 		while (true) {
 			ESD.sleep(1000);
@@ -55,12 +80,12 @@ public class AisLayer extends OMGraphicHandlerLayer implements Runnable, IVessel
 	private void drawVessels() {
 		if (aisHandler != null) {
 			list.clear();
-			
+
 			// Size of triangle
 			int[] xPos = { -sizeOffset, 0, sizeOffset, -sizeOffset };
 			int[] yPos = { -sizeOffset, sizeOffset, -sizeOffset, -sizeOffset };
-			
-			List<AisMessageExtended> shipList = aisHandler.getShipList();
+
+			shipList = aisHandler.getShipList();
 			for (int i = 0; i < shipList.size(); i++) {
 				if (aisHandler.getVesselTargets().containsKey(shipList.get(i).MMSI)) {
 					AisMessageExtended vessel = shipList.get(i);
@@ -70,57 +95,54 @@ public class AisLayer extends OMGraphicHandlerLayer implements Runnable, IVessel
 					location = vesselTarget.getPositionData();
 					double lat = location.getPos().getLatitude();
 					double lon = location.getPos().getLongitude();
-					
+
 					// Draw vessel
 					poly = new OMPoly(location.getPos().getLatitude(), location.getPos().getLongitude(), xPos, yPos, 0);
 					poly.setFillPaint(new Color(0));
 					list.add(poly);
-					
+
 					// Add MMSI sign
-				    label = new OMText(0, 0, 0, 0, Long.toString(shipList.get(i).MMSI), font, OMText.JUSTIFY_CENTER);
-				    label.setLat(lat);
-				    label.setLon(lon);
-				    label.setY(4*sizeOffset);
-				    label.setData("ID: " + Long.toString(vesselTarget.getMmsi()));
-				    list.add(label);
-				    
-				    if(staticData != null){
-				    	callSign = staticData.getCallsign();
-				    	name = staticData.getName();
-				    } else {
-				    	callSign = "N/A";
-				    	name = "N/A";
-				    }
-				    
-				    // Add call sign
-				    label = new OMText(0, 0, 0, 0, Long.toString(shipList.get(i).MMSI), font, OMText.JUSTIFY_RIGHT);
-				    label.setLat(lat);
-				    label.setLon(lon);
-				    label.setX(-2*sizeOffset);
-				    label.setY(sizeOffset);
-				    label.setData("Call Sign: " + callSign);
-				    list.add(label);
-				    
-				    // Add call sign
-				    label = new OMText(0, 0, 0, 0, Long.toString(shipList.get(i).MMSI), font, OMText.JUSTIFY_LEFT);
-				    label.setLat(lat);
-				    label.setLon(lon);
-				    label.setX(2*sizeOffset);
-				    label.setY(sizeOffset);
-				    label.setData("Name: " + name);
-				    list.add(label);
-				    
-				    // Add heading
-				    float trueHeading = location.getTrueHeading();
-				    System.out.println(trueHeading);
-				    boolean noHeading = false;
-			        if (trueHeading == 511) {
-			            trueHeading = location.getCog();
-			            noHeading = true;
-			        }
-			        
-				    
-				    
+					label = new OMText(0, 0, 0, 0, Long.toString(shipList.get(i).MMSI), font, OMText.JUSTIFY_CENTER);
+					label.setLat(lat);
+					label.setLon(lon);
+					label.setY(4 * sizeOffset);
+					label.setData("ID: " + Long.toString(vesselTarget.getMmsi()));
+					list.add(label);
+
+					if (staticData != null) {
+						callSign = staticData.getCallsign();
+						name = staticData.getName();
+					} else {
+						callSign = "N/A";
+						name = "N/A";
+					}
+
+					// Add call sign
+					label = new OMText(0, 0, 0, 0, Long.toString(shipList.get(i).MMSI), font, OMText.JUSTIFY_RIGHT);
+					label.setLat(lat);
+					label.setLon(lon);
+					label.setX(-2 * sizeOffset);
+					label.setY(sizeOffset);
+					label.setData("Call Sign: " + callSign);
+					list.add(label);
+
+					// Add call sign
+					label = new OMText(0, 0, 0, 0, Long.toString(shipList.get(i).MMSI), font, OMText.JUSTIFY_LEFT);
+					label.setLat(lat);
+					label.setLon(lon);
+					label.setX(2 * sizeOffset);
+					label.setY(sizeOffset);
+					label.setData("Name: " + name);
+					list.add(label);
+
+					// Add heading
+					/*
+					 * float trueHeading = location.getTrueHeading();
+					 * System.out.println(trueHeading); boolean noHeading =
+					 * false; if (trueHeading == 511) { trueHeading =
+					 * location.getCog(); noHeading = true; }
+					 */
+
 				}
 			}
 			doPrepare();
@@ -152,4 +174,72 @@ public class AisLayer extends OMGraphicHandlerLayer implements Runnable, IVessel
 		// TODO Auto-generated method stub
 
 	}
+
+	public void shipInfoWindow() {
+		List<AisMessageExtended> shipList = aisHandler.getShipList();
+
+		jf = new JFrame();
+		jf.setTitle("My frame");
+		jf.setSize(800, 600);
+		pane = jf.getContentPane();
+		pane.setLayout(new GridLayout(1, 3));
+
+		String fields[] = { "MSI" };
+		data = new String[shipList.size() + 1][1];
+		for (int i = 0; i < shipList.size(); i++) {
+			if (aisHandler.getVesselTargets().containsKey(shipList.get(i).MMSI)) {
+				if(shipList.get(i).MMSI > 219652000 && shipList.get(i).MMSI < 219654000){
+					System.out.println("YO!");
+					data[i][0] = Long.toString(shipList.get(i).MMSI);
+				}
+			}
+		}
+
+		table = new JTable(data, fields);
+		table.setBounds(0, 0, 100, 600);
+		JScrollPane test = new JScrollPane(table);
+		test.setBounds(0, 0, 100, 600);
+		table.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				if (SwingUtilities.isLeftMouseButton(e)) {
+					updateRightPaneOfInfoWindow(e.getPoint());
+				}
+			}
+		});
+		pane.add(test, 0);
+		tf2.setText("COMING..");
+		tf2.setEditable(false);
+		pane.add(tf2, 1);
+
+		JButton reload = new JButton();
+		reload.setText("Reload");
+		reload.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				jf.setVisible(false);
+				jf.dispose();
+				shipInfoWindow();
+			}
+		});
+		pane.add(reload, 2);
+
+		jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE);
+		jf.setVisible(true);
+	}
+
+	public void updateRightPaneOfInfoWindow(Point point) {
+		Long MMSI = Long.parseLong(data[table.rowAtPoint(point)][0]);
+
+		VesselTarget vesselTarget = aisHandler.getVesselTargets().get(MMSI);
+		VesselStaticData staticData = vesselTarget.getStaticData();
+
+		if (staticData != null) {
+			callSign = staticData.getCallsign();
+			name = staticData.getName();
+		} else {
+			callSign = "N/A";
+			name = "N/A";
+		}
+		tf2.setText("MMSI: "+data[table.rowAtPoint(point)][0]+"\nCall sign: "+callSign+"\nName: "+name);
+	}
+
 }
